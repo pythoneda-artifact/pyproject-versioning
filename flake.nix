61ba066e91a16728c14aad48343b7e16d5106772
rydnr <github@acm-sl.org>
2023-07-11 02:33:36 +0200

    Added pythoneda-artifact-event/versioning dependency

--- flake.nix
+++ flake.nix
@@ -15,6 +15,12 @@
       inputs.flake-utils.follows = "flake-utils";
       inputs.pythoneda-base.follows = "pythoneda-base";
     };
+    pythoneda-artifact-event-versioning = {
+      url = "github:pythoneda-artifact-event/versioning/0.0.1a1";
+      inputs.nixos.follows = "nixos";
+      inputs.flake-utils.follows = "flake-utils";
+      inputs.pythoneda-base.follows = "pythoneda-base";
+    };
   };
   outputs = inputs:
     with inputs;
@@ -30,7 +36,8 @@
         shared = import ./nix/shared.nix;
         pythonpackage = "pythonedaartifactpyprojectversioning";
         pythoneda-artifact-pyproject-versioning-for = { version, pythoneda-base
-          , pythoneda-artifact-shared-nix-flakes, python }:
+          , pythoneda-artifact-shared-nix-flakes
+          , pythoneda-artifact-event-versioning, python }:
           let
             pythonVersionParts = builtins.splitVersion python.version;
             pythonMajorVersion = builtins.head pythonVersionParts;
@@ -48,6 +55,7 @@
 
             nativeBuildInputs = with python.pkgs; [ pip pkgs.jq poetry-core ];
             propagatedBuildInputs = with python.pkgs; [
+              pythoneda-artifact-event-versioning
               pythoneda-artifact-shared-nix-flakes
               pythoneda-base
             ];
@@ -60,6 +68,7 @@
               python -m venv .env
               source .env/bin/activate
               pip install ${pythoneda-base}/dist/pythoneda_base-${pythoneda-base.version}-py${pythonMajorVersion}-none-any.whl
+              pip install ${pythoneda-artifact-event-versioning}/dist/pythoneda_artifact_event_versioning-${pythoneda-artifact-event-versioning.version}-py${pythonMajorVersion}-none-any.whl
               pip install ${pythoneda-artifact-shared-nix-flakes}/dist/pythoneda_artifact_shared_nix_flakes-${pythoneda-artifact-shared-nix-flakes.version}-py${pythonMajorVersion}-none-any.whl
               rm -rf .env
             '';
@@ -75,11 +84,13 @@
               inherit description homepage license maintainers;
             };
           };
-        pythoneda-artifact-pyproject-versioning-0_0_1a2-for =
-          { pythoneda-base, pythoneda-artifact-shared-nix-flakes, python }:
+        pythoneda-artifact-pyproject-versioning-0_0_1a2-for = { pythoneda-base
+          , pythoneda-artifact-event-versioning
+          , pythoneda-artifact-shared-nix-flakes, python }:
           pythoneda-artifact-pyproject-versioning-for {
             version = "0.0.1a2";
-            inherit pythoneda-base pythoneda-artifact-shared-nix-flakes python;
+            inherit pythoneda-base pythoneda-artifact-shared-nix-flakes
+              pythoneda-artifact-event-versioning python;
           };
       in rec {
         packages = rec {
@@ -87,6 +98,8 @@
             pythoneda-artifact-pyproject-versioning-0_0_1a2-for {
               pythoneda-base =
                 pythoneda-base.packages.${system}.pythoneda-base-latest-python38;
+              pythoneda-artifact-event-versioning =
+                pythoneda-artifact-event-versioning.packages.${system}.pythoneda-artifact-event-versioning-latest-python38;
               pythoneda-artifact-shared-nix-flakes =
                 pythoneda-artifact-shared-nix-flakes.packages.${system}.pythoneda-artifact-shared-nix-flakes-latest-python38;
               python = pkgs.python38;
@@ -95,6 +108,8 @@
             pythoneda-artifact-pyproject-versioning-0_0_1a2-for {
               pythoneda-base =
                 pythoneda-base.packages.${system}.pythoneda-base-latest-python39;
+              pythoneda-artifact-event-versioning =
+                pythoneda-artifact-event-versioning.packages.${system}.pythoneda-artifact-event-versioning-latest-python39;
               pythoneda-artifact-shared-nix-flakes =
                 pythoneda-artifact-shared-nix-flakes.packages.${system}.pythoneda-artifact-shared-nix-flakes-latest-python39;
               python = pkgs.python39;
@@ -103,6 +118,8 @@
             pythoneda-artifact-pyproject-versioning-0_0_1a2-for {
               pythoneda-base =
                 pythoneda-base.packages.${system}.pythoneda-base-latest-python310;
+              pythoneda-artifact-event-versioning =
+                pythoneda-artifact-event-versioning.packages.${system}.pythoneda-artifact-event-versioning-latest-python310;
               pythoneda-artifact-shared-nix-flakes =
                 pythoneda-artifact-shared-nix-flakes.packages.${system}.pythoneda-artifact-shared-nix-flakes-latest-python310;
               python = pkgs.python310;
